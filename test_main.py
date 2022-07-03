687fa442a3864cb680e303fc4e5ff500bc9289e7
dushiyu1 <57928680+dushiyu1@users.noreply.github.com>
2022-07-03 23:55:32 +0800

    Update and rename main.py to test_main.py

---
+++ test_main.py
@@ -0,0 +1,15 @@
+import pytest
+ 
+class TestClass:
+    def test_one(self):
+        x = "this"
+        assert "h" in x
+ 
+    def test_two(self):
+        x = "hello"
+        assert hasattr(x, "check")
+ 
+if __name__ == '__main__':
+#    pytest.main()
+    pytest.main(["./"])
+
